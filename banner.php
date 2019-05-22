c058eba0fa2f057261d8d507620dfe792603257c
Janarthanana <50097798+Janarthanana@users.noreply.github.com>
2019-05-22 15:32:16 +0530

    This is just testing pushes

---
+++ banner.php
@@ -0,0 +1,7 @@
+<div>
+	 <h1>Master in Epidemiology and Public Health</h1>
+	 <h4>Module I. Compulsory training</h4>
+	 <h1>Environmental risks in public health</h1>
+	 <h5>4ECTS</h5>
+	 <h6 class="text-right">Robert Jr</h6> 
+</div>
